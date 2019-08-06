a24370d32d6c7b259e3594ab20139370d2b19231
lei-diva <you@example.com>
2019-08-06 17:12:24 +0000

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a cycle in linked list
+ * @list: listint_t
+ * Return: 1 if founs, 0 if not)
+ */
+
+int check_cycle(listint_t *list)
+{
+
+	listint_t *tor = list;
+	listint_t *har = list;
+
+
+while (tor && har)
+{
+	if (tor->next == har->next->next)
+		return (1);
+	tor = tor->next;
+	har = har->next->next;
+}
+
+return (0);
+
+
+}
