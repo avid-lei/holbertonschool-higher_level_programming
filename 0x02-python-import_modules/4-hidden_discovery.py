fc7794e1d494279cfaca5daf1d2d54b17b7b5fb5
lei-diva <you@example.com>
2019-08-07 10:08:25 +0000

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    x = len(dir(hidden_4))
+    for i in range(x):
+            if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
+                print(dir(hidden_4)[i])
