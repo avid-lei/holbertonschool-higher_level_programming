f7f86e3b0265e865f59a10cd1e9296bf7c804be1
lei-diva <you@example.com>
2019-08-07 10:10:25 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,6 @@ import hidden_4
 if __name__ == "__main__":
     x = len(dir(hidden_4))
     for i in range(x):
-            if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
-                print(dir(hidden_4)[i])
+            if dir(hidden_4)[i][0] == '_' and dir(hidden_4)[i][1] == '_':
+                continue
+            print(dir(hidden_4)[i])
