f1d2875f86b73bf573127af63c8a7e4a464e35be
lei-diva <you@example.com>
2019-08-07 01:00:40 +0000

    pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for num1 in range(0, 10):
-	for num2 in range(num1 + 1, 10):
-		print("{:d}".format(num1), end="")
-		if num1 == 8:
-	  		  print("{:d}".format(num2))
-		else:
-			    print("{:d}, ".format(num2), end="")
+    for num2 in range(num1 + 1, 10):
+        print("{:d}".format(num1), end="")
+        if num1 == 8:
+            print("{:d}".format(num2))
+        else:
+            print("{:d}, ".format(num2), end="")
