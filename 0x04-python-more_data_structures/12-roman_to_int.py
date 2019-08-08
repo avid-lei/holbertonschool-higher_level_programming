2f74d00071762477c5601b23bc217034f9f6425b
lei-diva <you@example.com>
2019-08-08 19:40:16 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
+def convert(letter):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for key in roman:
+        if letter == key:
+            return roman[key]
+
+
 def roman_to_int(roman_string):
+
     if roman_string is None or roman_string == {}:
         return None
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
     sum = 0
-    for letter in roman_string:
-        for key in roman:
-            if letter == key:
-                sum = sum + roman[key]
+    prev = 0
+    for idx in range(0, len(roman_string)):
+        val1, val2 = 0, 0
+        val1 = convert(roman_string[idx])
+        if prev == 0:
+            if idx + 1 < len(roman_string):
+                val2 = convert(roman_string[idx + 1])
+                if val2 > val1:
+                    prev = val1
+                else:
+                    sum = sum + val1
+            else:
+                sum = sum + val1
+                break
+
+        else:
+            sum = sum + val1 - prev
+            prev = 0
+
     return sum
