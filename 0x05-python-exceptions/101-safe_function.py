6a5a18ab81b557264cf5083430417eb9f8289f6e
lei-diva <you@example.com>
2019-09-27 16:05:14 +0000

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        fct(*args)
+    except Exception as err:
+        sys.stderr.write("Exception: " + str(err) + "\n")
+        return None
+    return fct(*args)
