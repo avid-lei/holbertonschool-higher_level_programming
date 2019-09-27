5bf373f094c5e9756d8d5b9e6a203a8a9b25eab2
lei-diva <you@example.com>
2019-09-27 14:49:42 +0000

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quo = a / b
+    except ZeroDivisionError:
+        quo = None
+        return None
+    finally:
+        print("Inside result: {}".format(quo))
+        return quo
