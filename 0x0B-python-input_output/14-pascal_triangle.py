59c5c61ca4b056d5f6cc9f6e88c4f13e90ac2433
lei-diva <you@example.com>
2019-09-29 21:59:43 +0000

    14-pascal_triangle.py

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -8,10 +8,15 @@ return lists of lists of integers
 
 
 def pascal_triangle(n):
-	tri = [[]]
-	if n <= 0:
-		return tri
-	for i in range(1 : n + 1):
-		for i
+    tri = [[]]
+    if n <= 0:
+        return tri
+    for li in range(1, n + 1):
+        tri.append([])
+        for ele in range(li):
+            if ele == 0 or ele == li - 1:
+                tri[li].append(1)
+            else:
+                tri[li].append(tri[li-1][ele] + tri[li-1][ele-1])
 
-
+    return tri[1:]
