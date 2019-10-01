664d5bb13e4e0022b57a1e8206cf748c4f337efb
lei-diva <you@example.com>
2019-10-01 02:07:50 +0000

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """square clas"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialization of square"""
         super().__init__(size, size, x, y, id)
