b935102ded9e596f763f8f7e9dc4dd8727524358
lei-diva <you@example.com>
2019-10-01 02:00:33 +0000

    Sq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-from models.base import Base
-from models.rectangle import Rectangle
-
 """
 Square class
 """
+from models.rectangle import Rectangle
+from models.base import Base
 
 
 class Square(Rectangle):
