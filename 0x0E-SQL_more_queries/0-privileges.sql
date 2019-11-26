07a0a29ec6bf44d270412dafc16b7a6a9e7fd8c4
lei-diva <divaaleii@gmail.com>
2019-11-26 22:23:28 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- MySQL script to list all privileges
 -- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-SHOW GRANT FOR user_0d_1;
-SHOW GRANT FOR user_0d_2;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
