6a776c6001f7f78b57272f132dae2610ad7515be
lei-diva <divaaleii@gmail.com>
2019-11-26 22:50:30 +0000

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- MySQL script to create user with password
+-- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+GRANT ALL ON `database`.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
