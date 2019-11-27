7d81c0cb67f83549cf117ea57db771271dc4726e
lei-diva <divaaleii@gmail.com>
2019-11-27 02:35:33 +0000

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- list all shows contained in database
+-- cat 11-genre_id_all_shows.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
