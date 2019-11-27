54fb451dda7b5d0165ffed0a988a39d42fd6b6e5
lei-diva <divaaleii@gmail.com>
2019-11-27 07:32:42 +0000

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- list all shows and genres linked to show
 -- cat 16-shows_by_genre.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows JOIN tv_show_genres
+FROM tv_show_genres RIGHT JOIN  tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
