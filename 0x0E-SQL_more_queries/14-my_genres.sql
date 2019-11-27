f9564b9b1dcd7047788091d27c2badce7f5e9b6a
lei-diva <divaaleii@gmail.com>
2019-11-27 07:00:39 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,6 +4,6 @@ SELECT tv_genres.name
 FROM tv_show_genres JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres
-ON tv_show_genres.genres_id = tv_genres.id
+ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name;
