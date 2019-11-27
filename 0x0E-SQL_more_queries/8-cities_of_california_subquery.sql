0e358f191e23f81afb606e3d3cc9ea0b638bdd10
lei-diva <divaaleii@gmail.com>
2019-11-27 00:55:37 +0000

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- MySQL script to list all cities of CA found in database
 -- echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 USE hbtn_0d_usa;
-SELECT * FROM cities 
+SELECT * FROM cities, states
 WHERE state_id = 1
 ORDER BY id ASC;
