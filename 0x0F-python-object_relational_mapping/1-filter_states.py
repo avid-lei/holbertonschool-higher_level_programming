6f63d3395ee81fa7915ed376764b7afd50fab6bd
lei-diva <divaaleii@gmail.com>
2019-12-15 23:02:08 +0000

    case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                           passwd=sys.argv[2], db=sys.argv[3])
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
 
     rows = cur.fetchall()
 
