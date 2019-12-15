77f869e082ee6dac768d613f438dbcf7428ed944
lei-diva <divaaleii@gmail.com>
2019-12-15 23:16:16 +0000

    cons

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
                           passwd=argv[2], db=argv[3])
     cur = con.cursor()
 
-    sql = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(argv[4])
-    cur.execute(sql)
+
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(argv[4]))
 
     rows = cur.fetchall()
 
