35fd23e5c0adb738a559a50a7f70d85819fdb614
lei-diva <divaaleii@gmail.com>
2019-12-15 22:58:06 +0000

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    con = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                          passwd=sys.argv[2], db=sys.argv[3])
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    rows = cur.fetchall()
+
+    for eachrow in rows:
+        print(eachrow)
+
+    cur.close()
+    con.close()
