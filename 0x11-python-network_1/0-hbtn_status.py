8456f77c148acdd0152436a97b544bf8cc1832d9
lei-diva <divaaleii@gmail.com>
2020-01-13 22:37:04 +0000

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """python script fetches website"""
 
-import urllib.request
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as x:
-    d = x.read()
-    print('Body response:')
-    print(f'\t- type: {type(d)}')
-    print(f'\t- content: {d}')
-    print(f'\t- utf8 content: {d.decode("utf-8")}')
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as x:
+        d = x.read()
+        print('Body response:')
+        print(f'\t- type: {type(d)}')
+        print(f'\t- content: {d}')
+        print(f'\t- utf8 content: {d.decode("utf-8")}')
