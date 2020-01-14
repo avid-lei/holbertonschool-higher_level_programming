6fc6f2604d73765d79b244dc1f6759992a4c3586
lei-diva <divaaleii@gmail.com>
2020-01-14 07:32:57 +0000

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,8 +4,8 @@
 if __name__ == '__main__':
     import sys
     import requests
-    x = requests.get('https://api.github.com/repos/{}/{}/commits'.format
-                    (sys.argv[2], sys.argv[1]))
+    x = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
 
     js = x.json()[:10]
     for j in js:
