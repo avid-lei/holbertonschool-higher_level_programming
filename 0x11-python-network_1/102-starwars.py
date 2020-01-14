67ae0766a66da519d08872c26aef75215a128def
lei-diva <divaaleii@gmail.com>
2020-01-14 10:56:56 +0000

    102

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""star wars API"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    x = requests.get('https://swapi.co/api/people/',
+                     params={'search': sys.argv[1]})
+    j = x.json()
+    print('Number of results: {}'.format(j['count']))
+
+    total = j['count']
+    ctr = 0
+
+    while (ctr != total):
+        for i in j['results']:
+            print(i['name'])
+            ctr += 1
+            for f in i['films']:
+                fg = requests.get(f)
+                print('\t' + fg.json().get('title'))
+
+        if ctr == total:
+            break
+        x = requests.get(j['next'])
+        j = x.json()
