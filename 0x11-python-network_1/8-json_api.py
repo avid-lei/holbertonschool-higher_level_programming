827ba6902c6e5824df9886f06aaac0acc5f11f1f
lei-diva <divaaleii@gmail.com>
2020-01-14 03:04:24 +0000

    8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""search API with requests package"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    if len(sys.argv) < 2:
+        param = ""
+    else:
+        param = sys.argv[1]
+
+    x = requests.post('http://0.0.0.0:5000/search_user', data={'q': param})
+    try:
+        j = x.json()
+        if j == {}:
+            print('No result')
+        else:
+            print('[{}] {}'. format(j['id'], j['name']))
+    except ValueError:
+        print('Not a valid JSON')                                                                                                        ')
