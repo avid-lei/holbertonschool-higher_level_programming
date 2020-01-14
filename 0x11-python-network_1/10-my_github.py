785ca46cf377c568f7a36bf62a5751e57db236b4
lei-diva <divaaleii@gmail.com>
2020-01-14 05:49:33 +0000

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,7 +4,6 @@
 if __name__ == '__main__':
     import requests
     import sys
-    from requests.auth import HTTPBasicAuth
 
     g = requests.get('https://api.github.com/user',
                      auth=(sys.argv[1], sys.argv[2]))
