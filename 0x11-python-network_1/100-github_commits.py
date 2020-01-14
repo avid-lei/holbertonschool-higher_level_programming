4c04f9399521969d85f04eb63d99dfcb7ebfb74d
lei-diva <divaaleii@gmail.com>
2020-01-14 07:35:30 +0000

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,11 @@ if __name__ == '__main__':
     x = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(sys.argv[2], sys.argv[1]))
 
-    js = x.json()[:10]
+    js = x.json()
+    count = 0
     for j in js:
-        print('{}: {}'.format(j.get('sha'), j.get('commit')
-                              .get('author').get('name')))
+        print('{}: {}'.format(j.get('sha'),
+                              j.get('commit').get('author').get('name')))
+        count += 1
+        if count == 10:
+            break
