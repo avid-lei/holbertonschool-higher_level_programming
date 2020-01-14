a9477f7f070133b7969151cde3ace82641c67c6c
lei-diva <divaaleii@gmail.com>
2020-01-14 07:15:44 +0000

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Github Api to list recent commits
-"""
+"""Github Api to list recent commits"""
 
 if __name__ == '__main__':
     import sys
@@ -9,12 +7,9 @@ if __name__ == '__main__':
     x = requests.get('https://api.github.com/repos/{}/{}/commits'.format
                     (sys.argv[2], sys.argv[1]))
     j = x.json()
-
-    counter = 0
+    j = j[0:10]
 
     for x in j:
         print("{}: {}".format
              (x.get('sha'), x.get('commit').get('author').get('name')))
-        counter += 1
-        if counter == 10:
-            break
+
