c3ee860fc039aa5588b38be2679e4cb4296be904
lei-diva <divaaleii@gmail.com>
2020-02-12 03:31:07 +0000

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,20 @@
 #!/usr/bin/node
-request = require('request');
-request('https://swapi.co/api/people/18/', function (err, response, body) {
-  info = JSON.parse(body);
-  console.log(info.films.length);
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    let info = JSON.parse(body);
+    info = info.results;
+    let i = 0;
+    let count = 0;
+    while (info[i]) {
+      if (info[i].characters.indexOf('https://swapi.co/api/people/18/') > -1) {
+        count++;
+      }
+      i++;
+    }
+    console.log(count);
+  }
 });
