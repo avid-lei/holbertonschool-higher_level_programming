410ba13624285e2d5972c82b0e304d92cd39b5bb
lei-diva <divaaleii@gmail.com>
2020-02-12 04:06:04 +0000

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const emp = {};
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    const info = JSON.parse(body);
+    let i = 0;
+    while (info[i]) {
+      const userId = info[i].userId;
+      let count = 0;
+      let j = i;
+
+      while (info[j] && info[j].userId === userId) {
+        if (info[j].completed) {
+          count++;
+        }
+        j++;
+      }
+      emp[userId] = count;
+      i = j;
+    }
+    console.log(emp);
+  }
+});
